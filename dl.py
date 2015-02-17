84717a73e522312c10687d6a74a9ff95746a0b2b
Bill Mill <bill.mill@gmail.com>
2015-02-16 20:29:09 -0500

    initial commit

---
+++ dl.py
@@ -0,0 +1,34 @@
+import requests, json, shutil, time, hashlib, glob
+
+all = {}
+errorout = open("errors.log", "a")
+
+n = 0
+misses = 0
+playerurl = "http://fantasy.premierleague.com/web/api/elements/{}/"
+while 1:
+    r = requests.get(playerurl.format(n))
+    n += 1
+    if r.status_code != 200:
+        misses += 1
+        # if we get 25 misses in a row, assume we've found all players
+        if misses >= 25:
+            break
+        continue
+
+    misses = 0
+
+    if n%10 == 0: print(n)
+
+    try:
+        all[n] = r.json()
+    except ValueError:
+        print("failed parsing player {}".format(n))
+        errorout.write("Failed to parse player {}: {}\n".format(n, r.content))
+
+t = str(time.time()).split(".")[0]
+fn = "data/players.{}.json".format(t)
+with file(fn, 'w') as outfile:
+    json.dump(all, outfile, indent=2)
+
+shutil.copy2(fn, "players.current.json")
