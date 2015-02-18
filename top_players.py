3d1449a55d9f17d069ae6854e0df5d663dbfb7b5
Bill Mill <bill.mill@gmail.com>
2015-02-17 20:28:05 -0500

    add top_players.py

---
+++ top_players.py
@@ -0,0 +1,22 @@
+import json
+from collections import Counter
+import sys
+
+teams = json.load(open("top1k.current.json"))
+players = json.load(open("players.current.json"))
+startercount = Counter()
+benchcount = Counter()
+
+for teamid, (starters, bench) in teams.iteritems():
+    for playerid in starters:
+        player = players[playerid]
+        startercount[player["web_name"]] += 1
+    for playerid in bench:
+        player = players[playerid]
+        benchcount[player["web_name"]] += 1
+
+#for player, count in startercount.most_common():
+#    sys.stdout.write("{}: {}\n".format(player.encode("utf8"), count))
+
+for player, count in benchcount.most_common():
+    sys.stdout.write("{}: {}\n".format(player.encode("utf8"), count))
