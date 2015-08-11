8ff4c0eb7e5b37dabb2d3066c7bb62abc98aa8e9
Bill Mill <bill@billmill.org>
2015-08-10 22:26:14 -0400

    better reddit top players report

--- reddit_top_players.py
+++ reddit_top_players.py
@@ -7,6 +7,12 @@ allplayers = json.load(open("players.current.json"))
 startercount = Counter()
 benchcount = Counter()
 captains = Counter()
+positions = {
+    "Goalkeeper": Counter(),
+    "Defender": Counter(),
+    "Midfielder": Counter(),
+    "Forward": Counter(),
+}
 
 def get(playerid):
     return allplayers[str(playerid)]
@@ -14,6 +20,9 @@ def get(playerid):
 for teamid, players in teams.iteritems():
     for player in players:
         info = get(player["id"])
+        position = info["type_name"]
+        name = info["web_name"]
+        positions[position][name] += 1
         if player["sub"]:
             benchcount[info["web_name"]] += 1
         else:
@@ -21,17 +30,30 @@ for teamid, players in teams.iteritems():
         if player["captain"]:
             captains[info["web_name"]] += 1
 
-sys.stdout.write("------- captains ------\n")
 
-for player, count in captains.most_common():
-    sys.stdout.write("{}: {}\n".format(player.encode("utf8"), count))
+# Foo | Bar
+# ---|---
+# Foo | Bar
+sys.stdout.write("Goalkeeper | Count\n")
+sys.stdout.write("---------- | -----\n")
 
-sys.stdout.write("------- starters ------\n")
+for player, count in positions["Goalkeeper"].most_common(20):
+    sys.stdout.write("{} | {}\n".format(player.encode("utf8"), count))
 
-for player, count in startercount.most_common():
-    sys.stdout.write("{}: {}\n".format(player.encode("utf8"), count))
+sys.stdout.write("\nDefender | Count\n")
+sys.stdout.write("---------- | -----\n")
 
-sys.stdout.write("------- bench ------\n")
+for player, count in positions["Defender"].most_common(20):
+    sys.stdout.write("{} | {}\n".format(player.encode("utf8"), count))
 
-for player, count in benchcount.most_common():
-    sys.stdout.write("{}: {}\n".format(player.encode("utf8"), count))
+sys.stdout.write("\nMidfielder | Count\n")
+sys.stdout.write("---------- | -----\n")
+
+for player, count in positions["Midfielder"].most_common(20):
+    sys.stdout.write("{} | {}\n".format(player.encode("utf8"), count))
+
+sys.stdout.write("\nForward | Count\n")
+sys.stdout.write("---------- | -----\n")
+
+for player, count in positions["Forward"].most_common(20):
+    sys.stdout.write("{} | {}\n".format(player.encode("utf8"), count))
